cb942c08e33778755f0a3a7083fba83ddd07f4ed
Zulf Ahmed <zulf@pop-os.localdomain>
2021-01-23 19:40:58 -0600

    added inner outer regulatory lists

--- summary_virtues.R
+++ summary_virtues.R
@@ -1,15 +1,16 @@
 library(shiny)
 library(shinydashboard)
+
 ui <- dashboardPage(
   dashboardHeader(title = "Virtues Breakdown"),
   dashboardSidebar(),
   dashboardBody(
     # Boxes need to be put in a row (or column)
     fluidRow(
-      valueBox("inner")
-      valueBox("outer")
+      valueBox("inner"),
+      valueBox("outer"),
       valueBox("regulatory")
-    ) 
+    ),
     
     fluidRow(
       box(
@@ -20,13 +21,53 @@ ui <- dashboardPage(
   )
 )
 
+# inner Courage Wisdom
+inner_list <-c(
+"Creativity",
+"Curiosity",
+"Openmindedness",
+"LoveOfLearning",
+"Perspective",
+"Bravery",
+"Persistence",
+"Integrity",
+"Zest"
+)
+
+# outer Humanity Justice
+outer_list <-c(
+"Love",
+"Kindness",
+"SocialIntelligence",
+"Citizenship",
+"Fairness",
+"Leadership"
+)
+
+# regulatory Transcendence, Temperance
+regulatory_list <-c(
+"Forgiveness",
+"Humility",
+"Prudence",
+"Self-Regulation",
+"Appreciation",
+"Gratitude",
+"Hope",
+"Humor",
+"Spirituality"
+)
+
+percentile<-function( x, v ){
+  pnorm( x, mean=mean(v), sd=sd(v))
+}
+
 server <- function(input, output) {
   virtues_data <- dfp1
 
   j <- input$slider
-  inner_vals <- virtues_data[, inner_list]
-  outer_vals <- virtues_data[, outer_data]
-  regulatory_data <- virtues_data[, regulatory_list]
+  inner_vals <- RowSums(virtues_data[, inner_list])
+  outer_vals <- RowSums(virtues_data[, outer_data])
+  regulatory_data <- RowSums(virtues_data[, regulatory_list])
 
   inner_perc <- percentile( inner_vals[j], inner_vals )
   outer_perc <- percentile( outer_vals[j], outer_vals)
@@ -34,25 +75,27 @@ server <- function(input, output) {
   
   output$inner <- renderValueBox({
     valueBox(
-      paste0(inner_perc, "%"), "Inner Virtues", icon = icon("list"),
+      paste0(inner_perc, "%"), 
+      "Inner Virtues",
       color = "orange"
     )
   })
   
   output$inner <- renderValueBox({
     valueBox(
-      paste0(outer_perc), "%"), "Inner Virtues", icon = icon("list"),
+      paste0(outer_perc, "%"), 
+      "Inner Virtues", 
       color = "aqua"
     )
   })
 
   output$inner <- renderValueBox({
     valueBox(
-      paste0(regulatory_perc, "%"), "Regulatory Virtues", icon = icon("list"),
+      paste0(regulatory_perc, "%"), 
+      "Regulatory Virtues", 
       color = "green"
     )
   })
-
 }
 
 shinyApp(ui, server)
